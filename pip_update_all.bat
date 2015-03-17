26752ca38de08ee537953f112fcafb23de4aabda
wgordon <wgordon@masline.com>
2015-03-17 13:36:28 -0400

    Initial commit of Windows Batch script to update all installed packages

---
+++ pip_update_all.bat
@@ -0,0 +1,30 @@
+@ECHO off
+	rem Not exactly the fasted script in the world...likely due to network lag
+
+	rem Location of portably installed python and pip
+SET python=%cd%\python-3.4.3.amd64
+SET pip=%python%\Scripts\pip3.4.exe
+
+
+	rem Ensure localized working directory
+PUSHD %python%
+
+
+	rem Ensures portable python is used for pip as opposed to locally installed
+SET oldpath=%PATH%
+SET PATH=%python%\Lib\site-packages\PyQt4;%python%\;%python%\DLLs;%python%\Scripts;%python%\..\tools;%python%\..\tools\mingw32\bin;%python%\..\tools\R\bin\x64;%python%\..\tools\Julia\bin;%oldpath%
+
+
+	rem Loop through all portably installed packages and update if needed
+FOR /F "delims===" %%A IN ('%pip% freeze -l') DO (
+	%pip% install --no-cache-dir --upgrade --no-deps %%A
+	%pip% install --no-cache-dir %%A
+)
+
+
+	rem Restore old path
+SET PATH=%oldpath%
+
+
+	rem Restore initial working directory
+POPD
