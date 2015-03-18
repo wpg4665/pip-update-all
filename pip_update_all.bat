4ac29686ee99ae8fcda4d5d6e7b332bc92af448f
wgordon <wgordon@masline.com>
2015-03-18 16:44:40 -0400

    Added pip before/after comparison to provide information regarding what was upgraded

--- pip_update_all.bat
+++ pip_update_all.bat
@@ -9,6 +9,7 @@ SET FALSE=0
 	rem Read in extenal variables from pip_update_all.conf
 CALL :import_vars
 
+
 	rem Ensure localized working directory
 PUSHD "%python%"
 
@@ -17,8 +18,12 @@ PUSHD "%python%"
 SET PATH=%python%\Scripts;%python%;%PATH%
 
 
+	rem Output results of current pip freeze list, to be used for comparison after upgrades
+%pip% freeze > pip_freeze_results.before
+
+
 	rem Loop through all portably installed packages and update if needed
-FOR /F "delims===" %%A IN ('%pip% freeze') DO (
+FOR /F "delims===" %%A IN (pip_freeze_results.before) DO (
 	ECHO +++ Checking %%A for updates +++
 	%pip% install --no-cache-dir --upgrade --no-deps -q %%A > output.txt
 	TYPE output.txt 2>NUL | FIND "use --allow-external" >NUL 2>&1
@@ -33,12 +38,21 @@ FOR /F "delims===" %%A IN ('%pip% freeze') DO (
 			)
 		)
 	)
-	%pip% install --no-cache-dir -q %%A
+	%pip% install --no-cache-dir -q %%A >NUL 2>&1
 )
 
 
-	rem Delete potentially created error output message
+	rem Output results of new pip freeze list with upgrades
+%pip% freeze > pip_freeze_results.after
+
+	rem Compare the outputs of each pip freeze
+CALL :compare_freezes
+
+
+	rem Delete created files
 DEL /F /Q output.txt 2>NUL
+DEL /F /Q pip_freeze_results.before 2>NUL
+DEL /F /Q pip_freeze_results.after 2>NUL
 
 
 	rem Restore initial working directory
@@ -56,4 +70,25 @@ GOTO :eof
 		FOR /F "tokens=1,2 delims===" %%B IN ('TYPE pip_update_all.conf ^| FIND /V "#"') DO SET %%B=%%C
 	)
 	POPD
+GOTO :eof
+
+
+	rem Compare output of *.before and *.after files to output upgrade successes
+:compare_freezes
+	FOR /F "tokens=1,2 delims===" %%D IN (pip_freeze_results.after) DO (
+		SET found=%FALSE%
+		FOR /F "tokens=1,2 delims===" %%F IN (pip_freeze_results.before) DO (
+			rem Compare package name
+			IF "%%D"=="%%F" (
+				SET found=%TRUE%
+				rem Compare package version
+				IF "%%E" NEQ "%%G" (
+					ECHO %%D^(%%G^) -^> upgraded to %%E
+				)
+			)
+		)
+		IF "!found!"=="%FALSE%" (
+			ECHO %%D -^> installed @ %%E
+		)
+	)
 GOTO :eof
