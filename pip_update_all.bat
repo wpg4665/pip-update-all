14c107d7f5d41339e5252447592e7260e9938795
wgordon <wgordon@masline.com>
2015-03-18 16:57:20 -0400

    Turns out the `pip` variable was unnecessary when the PATH variable is overridden

--- pip_update_all.bat
+++ pip_update_all.bat
@@ -19,31 +19,31 @@ SET PATH=%python%\Scripts;%python%;%PATH%
 
 
 	rem Output results of current pip freeze list, to be used for comparison after upgrades
-%pip% freeze > pip_freeze_results.before
+pip freeze > pip_freeze_results.before
 
 
 	rem Loop through all portably installed packages and update if needed
 FOR /F "delims===" %%A IN (pip_freeze_results.before) DO (
 	ECHO +++ Checking %%A for updates +++
-	%pip% install --no-cache-dir --upgrade --no-deps -q %%A > output.txt
+	pip install --no-cache-dir --upgrade --no-deps -q %%A > output.txt 2>NUL
 	TYPE output.txt 2>NUL | FIND "use --allow-external" >NUL 2>&1
 	IF !ERRORLEVEL!==0 (
 		IF "%download_external%"=="%TRUE%" (
-			%pip% install --no-cache-dir --upgrade --no-deps -q --allow-external %%A %%A > output.txt
+			pip install --no-cache-dir --upgrade --no-deps -q --allow-external %%A %%A > output.txt 2>NUL
 			TYPE output.txt 2>NUL | FIND "use --allow-unverified" >NUL 2>&1
 			IF !ERRORLEVEL!==0 (
 				IF "%download_insecure%"=="%TRUE%" (
-					%pip% install --no-cache-dir --upgrade --no-deps -q --allow-external %%A --allow-unverified %%A %%A > output.txt
+					pip install --no-cache-dir --upgrade --no-deps -q --allow-external %%A --allow-unverified %%A %%A > output.txt 2>NUL
 				)
 			)
 		)
 	)
-	%pip% install --no-cache-dir -q %%A >NUL 2>&1
+	pip install --no-cache-dir -q %%A >NUL 2>&1
 )
 
 
 	rem Output results of new pip freeze list with upgrades
-%pip% freeze > pip_freeze_results.after
+pip freeze > pip_freeze_results.after
 
 	rem Compare the outputs of each pip freeze
 CALL :compare_freezes
